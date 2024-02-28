95898a3709c10446b7ca3853f48de24b63893b01
shebz2023 <shebelleke@gmail.com>
2024-02-29 00:55:54 +0200

    after improving tests

--- tests/all.test.ts
+++ tests/all.test.ts
@@ -1,24 +1,45 @@
 import request from 'supertest';
 import express from 'express';
+import mongoose from 'mongoose';
 import { config } from '../src/config/config';
 import todoRoutes from '../src/routes/Todo';
 import authRoutes from '../src/routes/auth';
 
 const app = express();
 
-// Middleware and routes setup
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/todos', todoRoutes);
+
+let token: string;
+
+beforeAll(async () => {
+  await mongoose.connect(config.mongo.url, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  } as mongoose.ConnectOptions);
+
+  const loginRes = await request(app)
+    .post('/auth/login')
+    .send({
+      email: 'user@gmail.com',
+      password: 'shebz@123',
+    });
+  token = loginRes.body.token;
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
 
 describe('Authentication Endpoints', () => {
   it('should register a new user', async () => {
     const res = await request(app)
       .post('/auth/register')
       .send({
-        username: 'testuser',
-        email: 'test@example.com',
-        password: 'password123',
+        username: 'shebz',
+        email: 'user@gmail.com',
+        password: 'shebz@123',
       });
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('token');
@@ -28,8 +49,8 @@ describe('Authentication Endpoints', () => {
     const res = await request(app)
       .post('/auth/login')
       .send({
-        email: 'test@example.com',
-        password: 'password123',
+        email: 'user@gmail.com',
+        password: 'shebz@123',
       });
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('token');
@@ -37,19 +58,6 @@ describe('Authentication Endpoints', () => {
 });
 
 describe('Todo Endpoints', () => {
-  let token: string;
-
-  beforeAll(async () => {
-    // Login to obtain token for authenticated requests
-    const loginRes = await request(app)
-      .post('/auth/login')
-      .send({
-        email: 'test@example.com',
-        password: 'password123',
-      });
-    token = loginRes.body.token;
-  });
-
   it('should create a new todo', async () => {
     const res = await request(app)
       .post('/todos')
@@ -70,11 +78,14 @@ describe('Todo Endpoints', () => {
     expect(Array.isArray(res.body)).toBeTruthy();
   });
 
-  // Add more test cases for other Todo endpoints (update, delete, etc.)
+  it('should fetch all ', async () => {
+    const res = await request(app)
+      .get('/')
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(404); 
+  });
 });
 
-// Start the server for testing
 app.listen(config.server.port, () => {
   console.log(`Test server is running on port ${config.server.port}`);
 });
-
